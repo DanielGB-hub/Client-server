8749fd0e1985359046cfcbcda9762d7c071211e1
Daniil_Gayvoronsky <qwer.82@mail.ru>
2021-03-10 21:09:38 +0300

    Update DZ3

--- data_client.py
+++ data_client.py
@@ -1,30 +1,72 @@
-#  1. Реализовать простое клиент-серверное взаимодействие по протоколу JIM
-#  (JSON instant messaging):
-
 from socket import *
+import click
+import time
 import json
 
 ENCODING = "utf-8"
 
-with socket(AF_INET, SOCK_STREAM) as s:
-    s.connect(("localhost", 8007))
 
-    auth_msg = {
+def form_auth_message():
+    msg_time = int(time.time())
+    data = {
         "action": "authenticate",
-        "time": 123,
+        "time": msg_time,
         "user": {
-                    "account_name": "C0deMaver1ck",
-                    "password": "CorrectHorseBatteryStaple"
+            "account_name": "C0deMaver1ck",
+            "account_password": "CorrectHorseBatteryStaple"
         }
     }
+    return json.dumps(data)
 
-    msg = json.dumps(auth_msg)
-    s.send(msg.encode(ENCODING))
-    data = s.recv(1000000)
-    recv_str = data.decode(ENCODING)
-    print("Сообщение от сервера: ", recv_str, ", длиной ", len(data), " байт")
-    s.close()
 
-    recv_msg = json.loads(recv_str)
-    print(f"Parsed msg {recv_msg}")
+def form_presence_message():
+    msg_time = int(time.time())
+    data = {
+        "action": "presence",
+        "time": msg_time,
+        "type": "status",
+        "user": {
+            "account_name": "C0deMaver1ck",
+            "account_password": "CorrectHorseBatteryStaple"
+        }
+    }
+    return json.dumps(data)
+
+
+def form_quit_message():
+    msg_time = int(time.time())
+    data = {
+        "action": "quit",
+        "time": msg_time,
+        "type": "exit",
+        "user": {
+            "account_name": "C0deMaver1ck",
+            "account_password": "CorrectHorseBatteryStaple"
+        }
+    }
+    return json.dumps(data)
+
+
+@click.command()
+@click.option("-port", default=7777, )
+@click.option("-addr", default="127.0.0.1", help="Адрес хоста (default=localhost)")
+def run_client(port, addr):
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((addr, port))
+        msg = form_auth_message()
+        s.send(msg.encode(ENCODING))
+        data = s.recv(1000000)
+        data = json.loads(data)
+        if data["response"] == 200:
+            msg2 = form_presence_message()
+            s.send(msg2.encode(ENCODING))
+
+            time.sleep(5)
+
+            msg3 = form_quit_message()  # отключаемся от сервера
+            s.send(msg3.encode(ENCODING))
+
+
+if __name__ == "__main__":
+    run_client()
 
--- data_server.py
+++ data_server.py
@@ -1,32 +1,53 @@
 #  1. Реализовать простое клиент-серверное взаимодействие по протоколу JIM
 #  (JSON instant messaging):
 
-from contextlib import closing
 from socket import *
+import click
 import json
+from contextlib import closing
 
 ENCODING = "utf-8"
 
-with socket(AF_INET, SOCK_STREAM) as s:
-    s.bind(("", 8007))
-    s.listen()
 
-    while True:
-        client, addr = s.accept()
-        with closing(client):
-            while True:
-                data = client.recv(1000000)
-                recv_str = data.decode(ENCODING)
-                print("Сообщение: ", recv_str, ", было отправлено клиентом: ", addr)
-                recv_msg = json.loads(recv_str)
+def form_auth_server_msg():
+    msg = {
+        "response": 200,
+        "alert": "Ok!"
+    }
+    return json.dumps(msg)
 
-                if "action" in recv_msg and recv_msg["action"] == "authenticate":
-                    msg = {
-                        "response": 200,
-                        "alert": "Необязательное сообщение/уведомление"
-                    }
-                    msg = json.dumps(msg)
-                    client.send(msg.encode(ENCODING))
-                elif "action" in recv_msg and recv_msg["action"] == "quit":
-                    print("Client exited")
-                    break
+
+@click.command()
+@click.option("-port", default=7777, help="порт (default=7777)")
+@click.option("-addr", default="", help="слушаемый ip адрес (default=все адреса)")
+def run_server(port, addr):
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((addr, port))
+        s.listen()
+
+        while True:
+            client, addr = s.accept()
+            with closing(client):
+                while True:
+                    data = client.recv(1000000)
+                    recv_str = data.decode(ENCODING)
+                    # print(f"\nСообщение: {recv_str}, было отправлено клиентом: {addr}\n")
+                    recv_msg = json.loads(recv_str)
+
+                    if "action" in recv_msg and recv_msg["action"] == "authenticate":
+                        print(f'Статус клиента {addr}: {recv_msg["action"]}')
+                        msg = form_auth_server_msg()
+                        client.send(msg.encode(ENCODING))
+
+                    if "action" in recv_msg and recv_msg["action"] == "presence":
+                        print(f'Статус клиента {addr} поменялся: {recv_msg["action"]}')
+
+                    elif "action" in recv_msg and recv_msg["action"] == "quit":
+                        print(f'Статус клиента {addr} поменялся: {recv_msg["action"]}')
+                        print("Клиент отключился от сервера, соедиение закрыто")
+                        break
+
+
+if __name__ == "__main__":
+    run_server()
+
